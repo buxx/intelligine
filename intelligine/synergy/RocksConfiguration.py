1f610488a2ada5a82f25c1d59e4876c4ba702b0e
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-02-12 07:56:50 +0100

    refact rocks configuration

--- intelligine/synergy/RocksConfiguration.py
+++ intelligine/synergy/RocksConfiguration.py
@@ -1,8 +1,5 @@
 from synergine.synergy.collection.Configuration import Configuration
 from intelligine.synergy.object.Rock import Rock
-from synergine.metas import metas
-from intelligine.cst import IMPENETRABLE
-from synergine.synergy.Simulation import Simulation
 
 
 class RocksConfiguration(Configuration):
@@ -10,46 +7,24 @@ class RocksConfiguration(Configuration):
     def get_start_objects(self, collection):
 
       rocks = []
+      rocks_positions = []
 
       for i in range(100):
-          rock = Rock()
-          metas.list.add(Simulation.STATE, rock.get_id(), IMPENETRABLE)
-          rock.set_position((0, 0+i, 0))
-          rocks.append(rock)
-
-      for i in range(100):
-          rock = Rock()
-          metas.list.add(Simulation.STATE, rock.get_id(), IMPENETRABLE)
-          rock.set_position((0, 0+i, 50))
-          rocks.append(rock)
+          rocks_positions.append((0, 0+i, 0))
+          rocks_positions.append((0, 0+i, 50))
 
       for i in range(50):
-          rock = Rock()
-          rock.set_position((0, 0, 0+i))
-          metas.list.add(Simulation.STATE, rock.get_id(), IMPENETRABLE)
-          rocks.append(rock)
-
-      for i in range(50):
+          rocks_positions.append((0, 0, 0+i))
           if i is not 25:
-            rock = Rock()
-            rock.set_position((0, 50, 0+i))
-            metas.list.add(Simulation.STATE, rock.get_id(), IMPENETRABLE)
-            rocks.append(rock)
+              rocks_positions.append((0, 50, 0+i))
+          rocks_positions.append((0, 100, 0+i))
 
-      for i in range(50):
+      rocks_positions.append((0, 50, 50))
+      rocks_positions.append((0, 100, 50))
+
+      for rock_position in rocks_positions:
           rock = Rock()
-          rock.set_position((0, 100, 0+i))
-          metas.list.add(Simulation.STATE, rock.get_id(), IMPENETRABLE)
+          rock.set_position(rock_position)
           rocks.append(rock)
-
-      rock = Rock()
-      rock.set_position((0, 50, 50))
-      metas.list.add(Simulation.STATE, rock.get_id(), IMPENETRABLE)
-      rocks.append(rock)
-
-      rock = Rock()
-      rock.set_position((0, 100, 50))
-      metas.list.add(Simulation.STATE, rock.get_id(), IMPENETRABLE)
-      rocks.append(rock)
 
       return rocks
--- intelligine/synergy/object/Rock.py
+++ intelligine/synergy/object/Rock.py
@@ -1,4 +1,11 @@
 from xyzworld.SynergyObject import SynergyObject as XyzSynergyObject
+from synergine.metas import metas
+from intelligine.cst import IMPENETRABLE
+from synergine.synergy.Simulation import Simulation
+
 
 class Rock(XyzSynergyObject):
-  pass
+
+    def __init__(self):
+        super().__init__()
+        metas.list.add(Simulation.STATE, self.get_id(), IMPENETRABLE)
