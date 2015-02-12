594473db9ff6b623a9c5a164718d743858518cba
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-02-12 07:49:00 +0100

    sandbox: dead cell images

--- intelligine/display/pygame/visualisation.py
+++ intelligine/display/pygame/visualisation.py
@@ -9,9 +9,14 @@ from os import getcwd
 from synergine.metas import metas
 from intelligine.cst import PREVIOUS_DIRECTION
 
+# TODO: Analyser les procedes ici pour proposer des outils dans le framework
+
 ant = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/ant.png')
+dead_ant = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/dead_ant.png')
 red_ant = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/red_ant.png')
 blue_ant = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/blue_ant.png')
+dead_red_ant = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/dead_red_ant.png')
+dead_blue_ant = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/dead_blue_ant.png')
 bug = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/ant.png')
 rock = PygameImage.from_filepath(getcwd()+'/intelligine/display/pygame/image/rock.png')
 
@@ -20,6 +25,8 @@ directions_red_ant = DirectionnedImage(red_ant)
 directions_blue_ant = DirectionnedImage(blue_ant)
 
 def bug_direction(bug):
+    if bug.get_life_points() <= 0:
+        return dead_ant
     try:
         previous_direction = metas.value.get(PREVIOUS_DIRECTION, bug.get_id())
     except KeyError:
@@ -27,6 +34,8 @@ def bug_direction(bug):
     return directions_ant.get_for_direction(previous_direction)
 
 def red_ant_direction(bug):
+    if bug.get_life_points() <= 0:
+        return dead_red_ant
     try:
         previous_direction = metas.value.get(PREVIOUS_DIRECTION, bug.get_id())
     except KeyError:
@@ -34,6 +43,8 @@ def red_ant_direction(bug):
     return directions_red_ant.get_for_direction(previous_direction)
 
 def blue_ant_direction(bug):
+    if bug.get_life_points() <= 0:
+        return dead_blue_ant
     try:
         previous_direction = metas.value.get(PREVIOUS_DIRECTION, bug.get_id())
     except KeyError:
