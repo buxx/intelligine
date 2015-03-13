6cf5e5b2b696934bd3a7e137ced56d6d1246fdeb
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-03-13 07:49:46 +0100

    Display pheromones when 'p' key pressed

--- intelligine/display/Pygame.py
+++ intelligine/display/Pygame.py
@@ -1,13 +1,20 @@
 from xyzworld.display.Pygame import Pygame as XyzPygame
+import pygame
 from intelligine.cst import PHEROMON_INFOS, PHEROMON_DIRECTION, PHEROMON_DIR_EXPLO, PHEROMON_POSITIONS
 from intelligine.synergy.object.ant.Pheromon import Pheromon
 
 
 class Pygame(XyzPygame):
 
+    def __init__(self, config, context):
+        super().__init__(config, context)
+        self._is_display_pheromones = False
+
+
     def receive(self, synergy_object_manager, context):
         super().receive(synergy_object_manager, context)
-        self._display_pheromones(context.metas.list.get(PHEROMON_POSITIONS, PHEROMON_POSITIONS, allow_empty=True), context)
+        if self._is_display_pheromones:
+            self._display_pheromones(context.metas.list.get(PHEROMON_POSITIONS, PHEROMON_POSITIONS, allow_empty=True), context)
 
     def _display_pheromones(self, pheromones_positions, context):
         for point in pheromones_positions:
@@ -31,3 +38,10 @@ class Pygame(XyzPygame):
                 pheromon = Pheromon(object(), context)
                 pheromon.set_direction(direction)
                 self._draw_objects_with_decal(point, [pheromon])
+
+    def _key_pressed(self, key):
+        if key == pygame.K_p:
+            if self._is_display_pheromones:
+                self._is_display_pheromones = False
+            else:
+                self._is_display_pheromones = True
