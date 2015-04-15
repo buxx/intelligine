562d029dceb30607ff7405601473b565688ce474
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-04-15 17:00:27 +0200

    add badges

--- intelligine/sandbox/exploration/ColonyConfiguration.py
+++ intelligine/sandbox/exploration/ColonyConfiguration.py
@@ -1,12 +1,9 @@
-from intelligine.core.exceptions import BestPheromoneHere
-from intelligine.simulation.pheromone.DirectionPheromone import DirectionPheromone
 from intelligine.synergy.ColonyConfiguration import ColonyConfiguration
 from intelligine.synergy.object.ant.Ant import Ant
-from intelligine.cst import PHEROMON_DIR_HOME
 
 
 class ColonyConfiguration(ColonyConfiguration):
 
-    _start_position = (0, 5, 5)
+    _start_position = (0, 1, 1)
     _ant_class = Ant
     _ant_count = 100
