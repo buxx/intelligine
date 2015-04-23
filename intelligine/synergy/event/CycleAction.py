dfaeb4ed6b63417e67639be1fbc79dd087f1f5c0
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-04-23 18:16:34 +0200

    indentation

--- intelligine/synergy/event/CycleAction.py
+++ intelligine/synergy/event/CycleAction.py
@@ -8,11 +8,10 @@ class CycleAction(Action):
     _listen = CycleEvent
 
     def run(self, obj, context, synergy_manager):
-      # TODO: refact ...
-      cant_carry_still = context.metas.value.get(CANT_CARRY_STILL, obj.get_id(), allow_empty=True, empty_value=0)
-      if cant_carry_still > 0:
-        context.metas.value.set(CANT_CARRY_STILL, obj.get_id(), cant_carry_still-1)
+        cant_carry_still = context.metas.value.get(CANT_CARRY_STILL, obj.get_id(), allow_empty=True, empty_value=0)
+        if cant_carry_still > 0:
+            context.metas.value.set(CANT_CARRY_STILL, obj.get_id(), cant_carry_still-1)
 
-      cant_put_still = context.metas.value.get(CANT_PUT_STILL, obj.get_id(), allow_empty=True, empty_value=0)
-      if cant_put_still > 0:
-        context.metas.value.set(CANT_PUT_STILL, obj.get_id(), cant_put_still-1)
+        cant_put_still = context.metas.value.get(CANT_PUT_STILL, obj.get_id(), allow_empty=True, empty_value=0)
+        if cant_put_still > 0:
+            context.metas.value.set(CANT_PUT_STILL, obj.get_id(), cant_put_still-1)
