737ca0b18286715ca633cdf6f21ca2bc1410493c
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-04-27 18:29:36 +0200

    update Display

--- intelligine/display/Pygame.py
+++ intelligine/display/Pygame.py
@@ -7,14 +7,15 @@ from intelligine.display.pygame.visualisation import SURFACE_PHEROMONE_EXPLORATI
 
 class Pygame(XyzPygame):
 
-    def __init__(self, config, context):
-        super().__init__(config, context)
+    def __init__(self, config, context, synergy_manager):
+        super().__init__(config, context, synergy_manager)
         self._is_display_pheromones = False
 
-    def receive(self, synergy_object_manager, context):
-        super().receive(synergy_object_manager, context)
+    def receive(self, actions_done):
+        super().receive(actions_done)
         if self._is_display_pheromones:
-            self._display_pheromones(context.metas.list.get(PHEROMON_POSITIONS, PHEROMON_POSITIONS, allow_empty=True), context)
+            pheromones_positions = self._context.metas.list.get(PHEROMON_POSITIONS, PHEROMON_POSITIONS, allow_empty=True)
+            self._display_pheromones(pheromones_positions, self._context)
 
     def _display_pheromones(self, pheromones_positions, context):
         pheromone_exploration_surface = self._object_visualizer.get_surface(SURFACE_PHEROMONE_EXPLORATION)
